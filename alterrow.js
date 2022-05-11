cea06ec810de9fc184d9b99bbd2cf866d687562c
Andreas Högvall <andreas.hogvall@atg.se>
2022-05-11 12:38:44 +0200

    Fixed pliga

--- alterrow.js
+++ alterrow.js
@@ -2,5 +2,5 @@ const styledRows = document.querySelectorAll('.table-row');
 const isOdd = n => Boolean(n % 2); // returns true if n / 2 has a remainder
 
 styledRows.forEach((row, i) => {
-    row.classList.add(isOdd(i) ? 'bg-slate-200' : 'bg-white');
+    row.classList.add(isOdd(i) ? 'bg-gray-300' : 'bg-white');
 });
